6b5b4e875ebd71cfd28858b0c327ec8322c3bc59
Tiodevs <Tiodevs>
2024-06-02 18:05:07 -0300

    Ajustes na responsividade do menu e do header

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,17 +1,57 @@
 import style from './menu.module.css'
 import logo from '../../assets/LogoFelipe.svg'
+import './hamburgers.css'
+import { useState } from 'react'
 
-export function Menu(){
-    return(
+export function Menu() {
+
+    const [isActive, setIsActive] = useState('hamburger hamburger--collapse')
+    const [menuMobi, setMenuMobi] = useState(style.menuMobi)
+
+    function handleIsActive() {
+        if (isActive === "hamburger hamburger--collapse") {
+            setIsActive("hamburger hamburger--collapse is-active")
+        } else {
+            setIsActive("hamburger hamburger--collapse")
+        }
+
+        if (menuMobi == style.menuMobi) {
+            setMenuMobi(style.menuMobiOpen)
+        } else {
+            setMenuMobi(style.menuMobi)
+        }
+    }
+
+    return (
         <nav className={style.menu}>
-            <img src={logo}/>
+            <div>
 
-            <ul>
-                <a>Projetos</a>
-                <a>Sobre mim</a>
-                <a>Contato</a>
-                <a>Meu CV</a>
+                <img src={logo} alt='Logo Header' />
+
+                <ul>
+                    <a href='.header'>Projetos</a>
+                    <a href='.header'>Sobre mim</a>
+                    <a href='.header'>Contato</a>
+                    <a href='.header'>Meu CV</a>
+                </ul>
+
+
+                <button class={isActive} type="button" onClick={handleIsActive}>
+                    <span class="hamburger-box">
+                        <span class="hamburger-inner"></span>
+                    </span>
+                </button>
+            </div>
+
+            <ul className={menuMobi}>
+                <a href='.header'>Projetos</a>
+                <a href='.header'>Sobre mim</a>
+                <a href='.header'>Contato</a>
+                <a href='.header'>Meu CV</a>
             </ul>
         </nav>
+
+
+
     )
 }
